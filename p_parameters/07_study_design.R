889322f54a8ffd978aea6be83f8eb585523370ea
Davide <davide.messina@ars.toscana.it>
2023-01-02 17:28:12 +0100

    Update spells. Everyone has one, except EFEMERIS/THL multiple

--- p_parameters/07_study_design.R
+++ p_parameters/07_study_design.R
@@ -32,3 +32,12 @@ gap_days <- ifelse(thisdatasource %not in% names(admissible_gap_obs_periods),
 
 # define number of days a spells should not be shorter
 min_spell_lenght <- 365
+
+# define from when the data is valid
+recommended_start_date_vect <- vector(mode = "list")
+recommended_start_date_vect[['ARS']] <- ymd(20030101)
+recommended_start_date_vect[['TEST']] <- ymd(20030101)
+
+recommended_start_date <- fifelse(thisdatasource %not in% names(recommended_start_date_vect),
+                                  ymd(19000101), recommended_start_date_vect[[thisdatasource]])
+rm(recommended_start_date_vect)
--- p_steps/01_T2_40_clean_spells.R
+++ p_steps/01_T2_40_clean_spells.R
@@ -3,32 +3,42 @@
 # input: D3_output_spells_category
 # output: D3_clean_spells
 
+# Load datasets
 smart_load("D3_PERSONS", dirtemp)
-
 smart_load("D3_output_spells_category", dirtemp)
 
+# Combine persons and spells, then select only the column we need and create new ones
 person_spell <- merge(D3_output_spells_category, D3_PERSONS, all.x = T, by = "person_id")
-
 person_spell <- person_spell[, .(person_id, birth_date, death_date, entry_spell_category_crude = entry_spell_category,
                                  exit_spell_category_crude = exit_spell_category, op_meaning, num_spell)]
 
-person_spell[, entry_spell_category := data.table::fifelse(birth_date < entry_spell_category_crude - 60,
-                                                           entry_spell_category_crude,
-                                                           birth_date)]
-person_spell[, exit_spell_category := pmin(exit_spell_category_crude, death_date, na.rm = T)]
+# If the spell start within 60 days from the birth the spell start becomes the birth date
+person_spell[, entry_spell_category := data.table::fifelse(birth_date < entry_spell_category_crude - 60 |
+                                                             birth_date > instance_creation,
+                                                           entry_spell_category_crude, birth_date)]
 
+# Censor spells which start before the recommended start date
+person_spell[, entry_spell_category := pmax(entry_spell_category, recommended_start_date, na.rm = T)]
+
+# Censor spells which start before the recommended start date
+person_spell[, entry_spell_category := pmax(entry_spell_category, recommended_start_date, na.rm = T)]
+
+# Create variable which says if the start/end of spell has been changed
 person_spell[, op_start_date_cleaned := data.table::fifelse(entry_spell_category != entry_spell_category_crude, 0, 1)]
 person_spell[, op_end_date_cleaned := data.table::fifelse(exit_spell_category != exit_spell_category_crude, 0, 1)]
 person_spell[, starts_at_birth := data.table::fifelse(entry_spell_category == birth_date, 1, 0)]
-person_spell[, starts_after_ending := data.table::fifelse(entry_spell_category < exit_spell_category, 0, 1)]
 
-# find spells that do not overlap the study period
-person_spell[, no_overlap_study_period := fifelse(
-  entry_spell_category > study_end | exit_spell_category < study_start, 1, 0)]
+# find spells that end before they start (using original start/end)
+person_spell[, starts_after_ending := data.table::fifelse(entry_spell_category_crude < exit_spell_category_crude, 0, 1)]
 
-# find spells that are shorter than x days
-person_spell[, less_than_x_days_and_not_starts_at_birth := fifelse(
-  correct_difftime(pmin(exit_spell_category, study_end), entry_spell_category) <= min_spell_lenght & starts_at_birth == 0, 1, 0)]
+# find spells that do not overlap the study period (using original start/end)
+person_spell[, no_overlap_study_period := data.table::fifelse(
+  entry_spell_category_crude > study_end | exit_spell_category_crude < study_start, 1, 0)]
+
+# find spells that are shorter than x days (using cleaned start/end)
+person_spell[, less_than_x_days_and_not_starts_at_birth := data.table::fifelse(
+  correct_difftime(exit_spell_category, entry_spell_category) < min_spell_lenght &
+    thisdatasource %not in% c("EFEMERIS", "THL"), 1, 0)]
 
 
 #TODO
@@ -45,14 +55,10 @@ person_spell[flag==0 & entry_spell_category >= study_start & exit_spell_category
 
 #alternative for dinamic cohort (example: if a subject enter after the study start it will be included with this alternative way)--------------------
 
-# On the spells with still flag equal to 0 take the one with the minimum exit_spell_category after study_start
-# person_spell[flag == 0 & exit_spell_category >= study_start,
-#              min_exit_spell_category := min(exit_spell_category), by = person_id]
-# 
-# person_spell[exit_spell_category == min_exit_spell_category, is_the_study_spell := 1]
-# person_spell[, c("min_exit_spell_category") := NULL]
-# 
-# person_spell[is.na(is_the_study_spell), is_the_study_spell := 0]
+# # Remove spells which are not last
+# person_spell[is_the_study_spell == 1, max_exit_spell_category := max(exit_spell_category), by = person_id]
+# person_spell[exit_spell_category != max_exit_spell_category, is_the_study_spell := 0]
+# person_spell[, c("max_exit_spell_category") := NULL]
 #----------------------------------
 
 ##add criteria to evaluate lookback 
